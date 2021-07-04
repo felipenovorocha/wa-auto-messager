aa317bf9a4cd50238d4a746dee55060256f690de
felipenovorocha <felipe.novo.rocha@gmail.com>
2021-07-04 14:55:30 -0300

    v1

---
+++ automessnger.py
@@ -0,0 +1,55 @@
+#%%
+
+#bibliotecas
+
+from selenium import webdriver
+import time
+from webdriver_manager.chrome import ChromeDriverManager
+import pyautogui as pg
+
+#navegar até o whatsapp
+
+#campo de pesquisa e mensagem - "copyable-text selectable-text"
+
+driver = webdriver.Chrome(ChromeDriverManager().install())
+driver.get("https://web.whatsapp.com/")
+contatos =["Thamires Silva Saraiva Rocha"]
+mensagem = "bom dia"
+
+time.sleep(15)
+
+
+def buscar_contato(contato):
+    campo_pesquisa=driver.find_element_by_xpath("//div[contains(@class,'copyable-text selectable-text')]")
+    time.sleep(3)
+    campo_pesquisa.click()
+    campo_pesquisa.send_keys(contato)
+    try:
+        campo_pesquisa.send_keys(Keys.ENTER)
+    except:
+        pg.press("Enter")
+
+def enviar_mensagem (mensagem):
+    campo_mensagem=driver.find_elements_by_xpath("//div[contains(@class,'copyable-text selectable-text')]")
+    time.sleep(3)
+    campo_mensagem[1].click()
+    time.sleep(1)
+    campo_mensagem[1].send_keys(mensagem)
+    time.sleep(1)
+    try:
+        campo_mensagem[1].send_keys(Keys.ENTER)
+    except:
+        pg.press("Enter")
+    
+    pass
+
+
+for contato in contatos:
+    buscar_contato(contato)
+    enviar_mensagem (mensagem)
+
+
+#definir contatos
+
+
+# %%
---
+++ readme.md
@@ -0,0 +1,11 @@
+<h1> Cheklist
+</h1>
+
+
+> - [ ] Importar biblioteca Selenium
+> - [ ] Importar biblioteca Pyautogui
+> - [ ] Fazer com que o Bot abra automaticamente o whatsapp
+> - [ ] Determinar nomes dos contatos
+> - [ ] Fazer com que o bot ache o chat através do xpath
+> - [ ] Determinar mensagem a ser escrita
+> - [ ] Enviar mensagem
